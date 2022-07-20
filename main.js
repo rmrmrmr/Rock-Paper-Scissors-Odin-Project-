31462077fa3bccf717d2c968c006e3a3a6e89908
rmrmrmr <ricardomartineza77@gmail.com>
2022-07-20 17:54:54 -0500

    fix game function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = prompt('Write here');
     let input = playerSelection.toLowerCase();
     let output = '';
-    
+
     if(input == 'rock') {
         switch(computerSelection) {
             case 'Rock':
@@ -21,50 +21,53 @@ function playRound(playerSelection, computerSelection) {
                 output += 'You win! Rock beats Scissors';
             }
         }
-        
-        if(input == 'paper') {
-            switch(computerSelection) {
-                case 'Rock':
+
+    if(input == 'paper') {
+        switch(computerSelection) {
+            case 'Rock':
                 output += "You win! Paper beats Rock";
                 break;
-                case 'Paper':
-                    output += "It's a tie!";
-                    break;
-                    case 'Scissors':
-                        output += 'You lose! Scissors beats Paper';
+            case 'Paper':
+                output += "It's a tie!";
+                break;
+            case 'Scissors':
+                output += 'You lose! Scissors beats Paper';
             }
         }
 
-        if(input == 'scissors') {
-            switch(computerSelection) {
-                case 'Rock':
-                    output += "You lose! Rock beats Scissors";
-                    break;
-                    case 'Paper':
-                        output += 'You win! Scissors beats Paper';
-                        break;
-                        case 'Scissors':
-                            output += "It's a tie!";
-                        }
-                    }
-                    return output;
-                }
-                
-                
-                
+    if(input == 'scissors') {
+        switch(computerSelection) {
+            case 'Rock':
+                output += "You lose! Rock beats Scissors";
+                break;
+            case 'Paper':
+                output += 'You win! Scissors beats Paper';
+                break;
+            case 'Scissors':
+                output += "It's a tie!";
+            }
+        }
+
+        return output;
+}
 
 function game() {
     let compScore = 0;
     let playerScore = 0;
+    let playedRounds = 0;
 
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; playedRounds < 4; i++){
         let round = playRound(null, getComputerChoice());
         console.log(round);
         if(round.includes('win') == true) {
             playerScore += 1;
+            playedRounds += 1;
+            if(playerScore == 3) break;
         }
         if(round.includes('lose') == true) {
             compScore += 1;
+            playedRounds += 1;
+            if(playerScore == 3) break;
         }
     }
 
@@ -76,4 +79,6 @@ function game() {
     }
 }
 
-console.log(game());
+game();
+
+
